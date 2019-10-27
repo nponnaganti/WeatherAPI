42e2d2366757630d4d03bf85825b35934eea2ad8
Nagendra Ponnaganti <nag.mrtn41@gmail.com>
2019-10-27 21:16:07 +1100

    added log4net

--- WeatherInfo/Controllers/WeatherFactory/CitiesService.cs
+++ WeatherInfo/Controllers/WeatherFactory/CitiesService.cs
@@ -12,8 +12,11 @@ namespace WeatherInfo.Controllers.WeatherFactory
 {
     public class CitiesService : ICitiesService
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(CitiesService));
         public List<SelectListItem> GetCitiesByQuery(string q)
         {
+            string methodName = "GetCitiesByQuery";
+            log.Info(methodName + " Has fired.");
             List<SelectListItem> selectListItems = new List<SelectListItem>();
 
             try
@@ -32,11 +35,14 @@ namespace WeatherInfo.Controllers.WeatherFactory
                     {
                         selectListItems.Add(new SelectListItem() { Text = item.AdministrativeArea.LocalizedName + ", " + item.Country.LocalizedName, Value = item.Key });
                     }
+                    log.Info(methodName + " Obsererd locations "+ selectListItems.Count() +" for the search text " +q);
+                    log.Info(methodName + " Successfully retured");
                     return selectListItems;
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                log.Error(methodName + " ", ex);
             }
             return selectListItems;
         }
