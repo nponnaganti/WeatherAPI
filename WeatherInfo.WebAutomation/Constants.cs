6a0fdbe27fbf705fc0e7a7c9bae63e0643a1a742
Nagendra Ponnaganti <nag.mrtn41@gmail.com>
2019-10-27 22:58:28 +1100

    Selenium QA Automation set up

---
+++ WeatherInfo.WebAutomation/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherInfo.WebAutomation
+{
+    public static class Constants
+    {
+        public static class Languages
+        {
+            public const string enUS = "en-us";
+        }
+        public static class Footer
+        {
+            public const string footerText = "© 2019 - My ASP.NET Application";
+        }
+    }
+}
---
+++ WeatherInfo.WebAutomation/IPageLayers.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherInfo.WebAutomation
+{
+    interface IPageLayers
+    {
+        void FooterText(IWebDriver driver);
+    }
+}
