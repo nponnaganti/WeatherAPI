4099ad15c6a60c6ea7d9dace9d5998368b3c48f9
Nagendra Ponnaganti <nag.mrtn41@gmail.com>
2019-10-27 22:20:51 +1100

    added empty projects for  Primitives, Interfaces,DataAccess,UnitTesting,WebAutomation

---
+++ WeatherInfo.Primitives/Constants.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherInfo.Primitives
+{
+    public static class Constants
+    {
+        public const string ConnectionStringKey = "WeatherApiConnectionString";
+        public const string DelimiterComma = ",";
+        public const string Schema = "[usp]";
+        public const string SystemSchema = "[dbo]";
+
+        public static class Columns
+        {
+
+        }
+        public static class DataTypes
+        {
+
+        }
+        public static class ErrorMessage
+        {
+
+        }
+        public static class Messages
+        {
+
+        }
+        public static class Parameters
+        {
+
+        }
+        public static class Procedures
+        {
+
+        }
+        public static class Rules
+        {
+
+        }
+        public static class Values
+        {
+
+        }
+    }
+}
